c14c3227a7282c9cde0e0bf4f95b8bdd5b28aa93
Stephen Weiss <stephen.c.weiss@gmail.com>
2025-05-12 09:12:53 -0500

    expand adjectives & nouns

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 #!/usr/bin/env node
 
+const adjectives = [
+  "Agile", "Brisk", "Brave", "Calm", "Clear", "Crisp", "Daring", "Eager", "Fierce", "Funny",
+  "Grand", "Happy", "Hardy", "Humble", "Jolly", "Keen", "Kind", "Large", "Lively", "Loud",
+  "Loyal", "Lucky", "Major", "Meek", "Mighty", "Minor", "Noble", "Odd", "Plucky", "Proud",
+  "Pure", "Quick", "Quiet", "Rapid", "Rare", "Regal", "Rich", "Rigid", "Rough", "Rugged",
+  "Silly", "Sincere", "Small", "Smart", "Snappy", "Solid", "Special", "Spunky", "Sturdy", "Sunny",
+  "Sweet", "Swift", "Tangy", "Tender", "Tense", "Tiny", "Tough", "Tricky", "True", "Twisty",
+  "Vast", "Vivid", "Wild", "Witty", "Wise", "Zany", "Zesty", "Breezy", "Bouncy", "Bossy",
+  "Crafty", "Cozy", "Curvy", "Dizzy", "Dapper", "Dreamy", "Feisty", "Fluffy", "Foggy", "Frosty",
+  "Funky", "Giddy", "Glossy", "Goofy", "Grainy", "Greedy", "Handy", "Icy", "Jazzy", "Jumpy",
+  "Juicy", "Loopy", "Moody", "Mushy", "Nutty", "Peppy", "Puffy", "Quirky", "Snug"
+];
+
+const nouns = [
+  "Elephant", "Rhinoceros", "Hippopotamus", "Bison", "Moose", "Gaur", "Giraffe", "Yak", "Camel", "Horse",
+  "Zebra", "Buffalo", "Tapir", "Warthog", "Wildebeest", "Caribou", "Okapi", "Reindeer", "Llama", "Vicuna",
+  "Saiga", "Antelope", "Elk", "Bear", "Kangaroo", "Walrus", "Polar Bear", "Grizzly Bear", "Koala", "Leopard",
+  "Tiger", "Lion", "Cheetah", "Jaguar", "Cougar", "Lynx", "Snow Leopard", "Oryx", "Sable", "Ocelot",
+  "Pronghorn", "Dromedary", "Addax", "Barbary Sheep", "Wild Boar", "Ibex", "Eland", "Giant Panda", "Sloth Bear", "Aurochs",
+  "Falcon", "Rocket", "Panther", "Comet", "Dragon", "Phoenix", "Cyclone", "Eagle", "Vortex", "Ninja",
+  "Yeti", "Kraken", "Shark", "Cobra", "Wolf", "Puma", "Tusk", "Talon", "Golem", "Sprite",
+  "Troll", "Ogre", "Imp", "Blaze", "Ember", "Spark", "Frost", "Storm", "Thunder", "Jet",
+  "Dash", "Bolt", "Flame", "Glitch", "Prism", "Pixel", "Pulse", "Orbit", "Drift", "Surge",
+  "Whirl", "Zephyr", "Echo", "Mirage", "Phantom", "Shadow", "Wisp"
+];
+
 /**
  * Sprint Namer - A tool for generating creative sprint names
  */
@@ -7,9 +33,6 @@
 function generateSprintName() {
   // This is a simple placeholder implementation
   // We'll expand this with more sophisticated logic later
-  const adjectives = ['Agile', 'Swift', 'Dynamic', 'Bold', 'Creative'];
-  const nouns = ['Journey', 'Sprint', 'Venture', 'Mission', 'Quest'];
-
   const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
   const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
 
